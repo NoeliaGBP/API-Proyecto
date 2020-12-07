80d688329ac471b26563bc929f6f714234780c46
Esmeralda137 <elara294jiutepec@gmail.com>
2020-12-07 13:15:17 -0600

    Se agregaron los pojos y daos correspondientes a Esmeralda

---
+++ Daily-Food-API/src/main/java/mx/edu/utez/menu/model/Menu.java
@@ -0,0 +1,43 @@
+package mx.edu.utez.menu.model;
+
+import mx.edu.utez.tipomenu.model.TipoMenu;
+
+public class Menu {
+    private int idMenu;
+    private String nombreMenu;
+    private TipoMenu idTipoMenu;
+    private Sucursal idSucursal;
+
+    public int getIdMenu() {
+        return idMenu;
+    }
+
+    public void setIdMenu(int idMenu) {
+        this.idMenu = idMenu;
+    }
+
+    public String getNombreMenu() {
+        return nombreMenu;
+    }
+
+    public void setNombreMenu(String nombreMenu) {
+        this.nombreMenu = nombreMenu;
+    }
+
+    public TipoMenu getIdTipoMenu() {
+        return idTipoMenu;
+    }
+
+    public void setIdTipoMenu(TipoMenu idTipoMenu) {
+        this.idTipoMenu = idTipoMenu;
+    }
+
+    public Sucursal getIdSucursal() {
+        return idSucursal;
+    }
+
+    public void setIdSucursal(Sucursal idSucursal) {
+        this.idSucursal = idSucursal;
+    }
+
+}
